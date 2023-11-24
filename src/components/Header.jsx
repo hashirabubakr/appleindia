f8861653b53d0a300a4eadceb06896c8b7258fa0
hashirabubakr <hashirabubakrr@gmail,com>
2023-11-24 16:42:19 +0530

    small edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,12 @@ function Header() {
     >
       <Container fluid>
         <Navbar.Brand className='d-flex align-items-center ms-5 '>
-          <i className="fab fa-apple ms-2" style={{ color: '#ffffff' }}></i>
+          <i className="fab fa-apple ms-2 me-2" style={{ color: '#ffffff' }}></i>
 
         </Navbar.Brand>
      
         <Navbar.Collapse className='bg-black'  id="responsive-navbar-nav">
-
-            <Nav.Link className='ms-5' href="#home">Store</Nav.Link>
+<Nav.Link className='ms-5' href="#home">Store</Nav.Link>
             <Nav.Link className='ms-5' href="#">Mac</Nav.Link>
             <Nav.Link className='ms-5' href="#">iPad</Nav.Link>
             <Nav.Link className='ms-5' href="#">iPhone</Nav.Link>
@@ -30,8 +29,8 @@ function Header() {
             <Nav.Link className='ms-5' href="#">Accessories</Nav.Link>
             <Nav.Link className='ms-5' href="#">Support</Nav.Link>
              </Navbar.Collapse>
-             <Nav.Link className='ms-5 me-5' href="#"><i className="fa-solid fa-search"></i></Nav.Link>
-            <Nav.Link className='ms-5 me-5' href="#"><i className="fa-solid fa-bag-shopping"></i></Nav.Link>
+             <Nav.Link href="#"><i className="fa-solid fa-search"></i></Nav.Link>
+            <Nav.Link className='ms-5 me-5' href="#"><i className="fa-solid fa-bag-shopping me-5"></i></Nav.Link>
             <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
       </Container>
     </Navbar>
