073f51c3a26d0356bb5ad0141f8f7c925b5b7bb4
hashirabubakr <hashirabubakrr@gmail,com>
2023-11-24 16:23:33 +0530

    navbar edited for sm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
 function Header() {
   return (
     <Navbar
       style={{ height: '55px' }}
       fixed="top"
-      className='bg-dark'
-      variant="dark"
+      className='bg-black'
+      variant="black"
       expand="xl"
     >
       <Container fluid>
         <Navbar.Brand className='d-flex align-items-center ms-5 '>
-          <i className="fab fa-apple ms-5" style={{ color: '#ffffff' }}></i>
-          
+          <i className="fab fa-apple ms-2" style={{ color: '#ffffff' }}></i>
+
         </Navbar.Brand>
-        <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse  id="responsive-navbar-nav">
-       
+     
+        <Navbar.Collapse className='bg-black'  id="responsive-navbar-nav">
+
             <Nav.Link className='ms-5' href="#home">Store</Nav.Link>
             <Nav.Link className='ms-5' href="#">Mac</Nav.Link>
             <Nav.Link className='ms-5' href="#">iPad</Nav.Link>
@@ -29,12 +28,11 @@ function Header() {
             <Nav.Link className='ms-5' href="#">TV & Home</Nav.Link>
             <Nav.Link className='ms-5' href="#">Entertainment</Nav.Link>
             <Nav.Link className='ms-5' href="#">Accessories</Nav.Link>
-            <Nav.Link className='ms-4' href="#">Support</Nav.Link>
-            <Nav.Link className='ms-4 ' href="#"><i className="fa-solid fa-search"></i></Nav.Link>
-            <Nav.Link className='ms-4' href="#"><i className="fa-solid fa-bag-shopping"></i></Nav.Link>
-           
-         
-        </Navbar.Collapse>
+            <Nav.Link className='ms-5' href="#">Support</Nav.Link>
+             </Navbar.Collapse>
+             <Nav.Link className='ms-5 me-5' href="#"><i className="fa-solid fa-search"></i></Nav.Link>
+            <Nav.Link className='ms-5 me-5' href="#"><i className="fa-solid fa-bag-shopping"></i></Nav.Link>
+            <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
       </Container>
     </Navbar>
   );
